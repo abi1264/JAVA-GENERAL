e25d504bd67a8d321ff70ec1ea5be0e9c2f39561
abi <thapaabi180@gmail.com>
2026-02-26 17:13:34 +0545

    New all added

---
+++ Result.java
@@ -0,0 +1,12 @@
+public class Result {
+    public int maths;
+    public int science;
+    public int english;
+
+    public Result(int maths,int science, int english){
+        this.maths=maths;
+        this.science=science;
+        this.english=english;
+    }
+    
+}
---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student{
+    public int a, b, c ;
+    public void  Studnet(){
+        a=10;
+        b=10;
+        c=a+b;
+
+    }
+}
